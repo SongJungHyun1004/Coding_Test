7a9f084ca4a5f3cd52c9e70df72e48109bda05c8
SongJungHyun <201802112@o.cnu.ac.kr>
2023-11-02 22:16:24 +0900

    add day123 coding_test code

---
+++ 18주차/체크판위에서 2.py
@@ -0,0 +1,13 @@
+r, c = map(int, input().split())
+board = [
+    input().split()
+    for _ in range(r)
+]
+cnt = 0
+for i in range(1, r):
+    for j in range(1, c):
+        for k in range(i+1, r-1):
+            for l in range(j+1, c-1):
+                if board[0][0] != board[i][j] and board[i][j] != board[k][l] and board[k][l] != board[-1][-1]:
+                    cnt += 1
+print(cnt)
